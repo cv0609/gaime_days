3d05bfe9f853e6d74bcd2c9b6acee9a904fedd98
karan <kn@int.iqsay.com>
2024-12-18 11:01:15 +0530

    fixed

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Soccer Spotlight</title>
+  <title>Gaime Days</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -51,7 +51,7 @@
               <div class="d-flex justify-content-center py-4">
                 <a href="#" class="logo d-flex align-items-center w-auto">
                   {{-- <img src="assets/img/logo.png" alt=""> --}}
-                  <span class="d-none d-lg-block">Soccer Spotlight</span>
+                  <span class="d-none d-lg-block">Gaime Days</span>
                 </a>
               </div><!-- End Logo -->
 
