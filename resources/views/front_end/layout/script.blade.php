c334bacb8699654e58592f07388df0620af2719c
karan <kn@int.iqsay.com>
2024-12-18 15:14:48 +0530

    fixed

--- resources/views/front_end/layout/script.blade.php
+++ resources/views/front_end/layout/script.blade.php
@@ -2,6 +2,5 @@
 <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
 <script src="{{ asset('assets/js/aos.js') }}"></script>
 <script src="{{ asset('assets/js/index.js') }}"></script>
-@if(route::is('contact.us'))
 <script src="//code.tidio.co/ndybfva6yxjetlq8nmctrflpcglucejc.js" async></script>
-@endif
+
