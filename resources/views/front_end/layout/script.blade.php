8586b7db1ba4b008ea751d8d1fbb66ec4a6e5496
karan <kn@int.iqsay.com>
2024-12-18 15:04:52 +0530

    fixed

--- resources/views/front_end/layout/script.blade.php
+++ resources/views/front_end/layout/script.blade.php
@@ -1,5 +1,7 @@
 <script src="{{ asset('assets/js/jquery.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('assets/js/aos.js') }}"></script>
-    <script src="{{ asset('assets/js/index.js') }}"></script>
-    <script src="//code.tidio.co/ndybfva6yxjetlq8nmctrflpcglucejc.js" async></script>
+<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+<script src="{{ asset('assets/js/aos.js') }}"></script>
+<script src="{{ asset('assets/js/index.js') }}"></script>
+@if(route::is('contact.us'))
+<script src="//code.tidio.co/ndybfva6yxjetlq8nmctrflpcglucejc.js" async></script>
+@endif
