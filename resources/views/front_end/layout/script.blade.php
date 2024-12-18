10752b8d15d9639d224b69e1abf299e670b844e4
karan <kn@int.iqsay.com>
2024-12-18 15:01:02 +0530

    fixed

--- resources/views/front_end/layout/script.blade.php
+++ resources/views/front_end/layout/script.blade.php
@@ -2,3 +2,4 @@
     <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
     <script src="{{ asset('assets/js/aos.js') }}"></script>
     <script src="{{ asset('assets/js/index.js') }}"></script>
+    <script src="//code.tidio.co/ndybfva6yxjetlq8nmctrflpcglucejc.js" async></script>
